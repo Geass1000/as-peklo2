709bea23f2f6206fbb7eac74e483e3226c96d50d
Geass <geass1000@gmail.com>
2017-06-28 02:23:28 +0300

    Add: Method and field for game account

--- src/app/actions/app.actions.ts
+++ src/app/actions/app.actions.ts
@@ -5,8 +5,15 @@ import { IAction } from '../shared/interfaces/action.interface';
 export class AppActions {
 	static readonly CLASS_NAME = 'AppActions:';
 	static readonly TOGGLE_FULL_WIDTH_MODE = AppActions.CLASS_NAME + 'TOGGLE_FULL_WIDTH_MODE';
+	static readonly SET_ACC = AppActions.CLASS_NAME + 'SET_ACC';
 	static readonly SET_SID = AppActions.CLASS_NAME + 'SET_SID';
 
+	setAcc (uid : string, auth_key : string, sid : string) : IAction {
+    return {
+      type : AppActions.SET_ACC,
+			payload : {	uid : uid, auth_key : auth_key, sid : sid }
+    };
+  }
 	setSid (sid : string) : IAction {
     return {
       type : AppActions.SET_SID,
--- src/app/reducers/state.reducer.ts
+++ src/app/reducers/state.reducer.ts
@@ -3,15 +3,26 @@ import { IAction } from '../shared/interfaces/action.interface';
 import { AppActions } from '../actions/app.actions';
 
 export interface IState {
+	uid : string;
+	auth_key : string;
 	sid : string;
 }
 
 export const INITIAL_STATE : IState = {
+	uid : '',
+	auth_key : '',
 	sid : ''
 };
 
 export const StateReducer : Reducer<IState> = (state = INITIAL_STATE, action : IAction) : IState => {
 	switch (action.type) {
+		case AppActions.SET_ACC : {
+			return Object.assign({}, state, {
+				uid : action.payload.uid,
+				auth_key : action.payload.auth_key,
+				sid : action.payload.sid
+			});
+		}
 		case AppActions.SET_SID : {
 			return Object.assign({}, state, {
 				sid : action.payload.sid
