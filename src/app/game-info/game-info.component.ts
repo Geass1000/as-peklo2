83e6e342fe01e3b6db18a3e8b8c37c7f1547a9a9
Geass <geass1000@gmail.com>
2017-07-01 14:19:02 +0300

    Add: Form system was reworked and balance was calculated

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -13,7 +13,10 @@ import { GameService } from '../core/game.service';
 
 /* App Interfaces and Classes */
 import { IState } from '../reducers/state.reducer';
-import { IRGameInfo, IResources, IArmory } from '../shared/interfaces/game.interface';
+import { IRGameInfo, IResources, IArmory, ICostArmory, ICost } from '../shared/interfaces/game.interface';
+
+/* App Validators */
+import { isNumber } from '../shared/validators/is-number.validator';
 
 const initModel : IArmory = {
 	air_strike : '0',
@@ -26,6 +29,17 @@ const initModel : IArmory = {
 	ecm : '0'
 };
 
+const costArmory : ICostArmory = {
+	air_strike : { type : 'crystal', cost : 30 },
+	medicaments : { type : 'crystal', cost : 30 },
+	gravibomb : { type : 'cordite', cost : 50 },
+	shields : { type : 'cordite', cost : 50 },
+	space_mines : { type : 'cordite', cost : 25 },
+	repair_drones : { type : 'cordite', cost : 25 },
+	adaptive_shield : { type : 'cordite', cost : 50 },
+	ecm : { type : 'cordite', cost : 50 }
+};
+
 @Component({
 	moduleId: module.id,
 	selector: 'as-game-info',
@@ -33,10 +47,8 @@ const initModel : IArmory = {
   styleUrls: [ 'game-info.component.scss' ]
 })
 export class GameInfoComponent implements OnInit, OnDestroy {
-	public metal : string;
-	public cristal : string;
-	public cordit : string;
-	public model : IArmory;
+	public balance : IResources;
+	form : FormGroup;
 
 	/* Redux */
 	private subscription : Array<Subscription> = [];
@@ -53,18 +65,63 @@ export class GameInfoComponent implements OnInit, OnDestroy {
 						 	 private gameService : GameService) { ; }
 
 	ngOnInit () : void {
+		this.buildForm();
 		this.subscription.push(this.stateResources$.subscribe((data) => {
-			this.stateResources = data;
+			this.stateResources = Object.assign({}, data);
+			this.balance = Object.assign({}, data);
 		}));
 		this.subscription.push(this.stateArmory$.subscribe((data) => {
-			this.stateArmory = data;
+			this.stateArmory = Object.assign({}, data);
 		}));
 		this.getInfo();
-		this.model = Object.assign({}, initModel);
   }
 	ngOnDestroy () : void {
 		this.subscription.map((data) => data.unsubscribe());
 	}
+
+	/**
+	 * buildForm - функция-метод, выполняет создание формы и возможные регистрации
+	 * на события формы.
+	 *
+	 * @function
+	 * @return {void}
+	 */
+	buildForm () : void {
+		this.form = this.fb.group({
+      'air_strike' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'medicaments' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'gravibomb' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'shields' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'space_mines' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'repair_drones' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'adaptive_shield' : [ '0', [ Validators.required, isNumber(false) ] ],
+			'ecm' : [ '0', [ Validators.required, isNumber(false) ] ],
+    });
+
+		this.form.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+	}
+
+	onValueChanged (data?: any) : void {
+		const methodName : string = 'onValueChanged';
+
+		if (this.form.invalid) {
+			this.logger.info(`${this.constructor.name} - ${methodName}:`, 'Form invalid');
+			return;
+		}
+
+		this.balance = Object.assign({}, this.stateResources);
+		const result : IArmory = <IArmory>Object.assign({}, this.form.value);
+		this.logger.info(`${this.constructor.name} - ${methodName}:`, result);
+
+		for (let prop in result) {
+			const value : number = +result[prop];
+			const cost : ICost = costArmory[prop];
+		  this.balance[cost.type] -= cost.cost * value;
+		}
+  }
 
 	/**
 	 * getUrlResouce - выполняет формирование URL изображения ресурсов.
@@ -115,8 +172,28 @@ export class GameInfoComponent implements OnInit, OnDestroy {
 		this.subscription.push(sub);
 	}
 
+	/**
+	 * onClickOrder - функция-событие, выполняет заказ и получение арсенала.
+	 *
+	 * @kind {event}
+	 * @return {void}
+	 */
 	onClickOrder () : void {
 		const methodName : string = 'onClickOrder';
-		this.logger.info(`${this.constructor.name} - onClickOrder:`, this.model);
+		if (this.form.invalid) {
+			this.logger.info(`${this.constructor.name} - ${methodName}:`, 'Form invalid');
+			return;
+		}
+		let confirm : boolean = true;
+		for(let prop in this.balance) {
+			confirm = confirm && this.balance[prop] >= 0;
+		}
+		if (!confirm) {
+			this.logger.info(`${this.constructor.name} - ${methodName}:`, 'All field mast be positive');
+			return;
+		}
+
+		const result : IArmory = <IArmory>Object.assign({}, this.form.value);
+		this.logger.info(`${this.constructor.name} - ${methodName}:`, result);
 	}
 }
