6664153d73b1a44466f99647cb4b0c69adf77f89
Geass <geass1000@gmail.com>
2017-07-02 03:27:50 +0300

    Add: Modal block was added

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -18,6 +18,9 @@ import { IRGameInfo, IResources, IArmory, ICostArmory, ICost, IRGameArmory, IGam
 /* App Validators */
 import { isNumber } from '../shared/validators/is-number.validator';
 
+/* App Animations */
+import { animation } from '../shared/animations/modal.animation';
+
 const initModel : IArmory = {
 	air_strike : '0',
 	medicaments : '0',
@@ -41,19 +44,24 @@ const costArmory : ICostArmory = {
 };
 
 @Component({
-	moduleId: module.id,
-	selector: 'as-game-info',
-	templateUrl: 'game-info.component.html',
-  styleUrls: [ 'game-info.component.scss' ]
+	moduleId : module.id,
+	selector : 'as-game-info',
+	templateUrl : 'game-info.component.html',
+  styleUrls : [ 'game-info.component.scss' ],
+	animations : [ animation ]
 })
 export class GameInfoComponent implements OnInit, OnDestroy {
 	public balance : IResources;
 	public resultArmory : IArmory;
-	form : FormGroup;
+	public animationState : string = 'open';
+	public form : FormGroup;
+	public waitMin : number;
+	public waitSec : number;
 
 	/* Redux */
 	private subscription : Array<Subscription> = [];
-
+	@select(['modal', 'gameInfo']) gameInfo$ : Observable<boolean>;
+	public gameInfo : boolean;
 	@select(['state', 'resources']) stateResources$ : Observable<IResources>;
 	public stateResources : IResources;
 	@select(['state', 'armory']) stateArmory$ : Observable<IArmory>;
@@ -70,6 +78,10 @@ export class GameInfoComponent implements OnInit, OnDestroy {
 
 	ngOnInit () : void {
 		this.buildForm();
+		this.subscription.push(this.gameInfo$.subscribe((data) => {
+			this.gameInfo = data;
+			this.animationState = data ? 'open' : '';
+		}));
 		this.subscription.push(this.stateResources$.subscribe((data) => {
 			this.stateResources = Object.assign({}, data);
 			this.balance = Object.assign({}, data);
@@ -204,6 +216,16 @@ export class GameInfoComponent implements OnInit, OnDestroy {
 
 		const result : IArmory = <IArmory>Object.assign({}, this.form.value);
 		this.logger.info(`${this.constructor.name} - ${methodName}:`, result);
+		let sum : number = 0;
+		for (let prop in result) {
+			sum += result[prop];
+		}
+		const perf : number = 0.2;
+		sum *= perf;
+		this.waitMin = Math.floor(sum / 60);
+		this.waitSec = Math.ceil(sum) % 60;
+
+		this.ngRedux.dispatch(this.appActions.openModal('gameInfo'));
 
 		const value = {
 			land : this.land,
@@ -213,9 +235,15 @@ export class GameInfoComponent implements OnInit, OnDestroy {
 		const sub : Subscription = this.gameService.postGetArmory(value).subscribe(
 			(data : IRGameArmory) => {
 				this.logger.info(`${this.constructor.name} - ${methodName}:`, data);
+				this.ngOnInit();
 			},
 			(error : string) => {
 				this.logger.info(`${this.constructor.name} - getInfo:`, error);
+			},
+			() => {
+				setTimeout(() => {
+					this.ngRedux.dispatch(this.appActions.closeActiveModal());
+				}, 1000);
 			}
 		);
 		this.subscription.push(sub);
