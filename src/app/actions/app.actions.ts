d5a6fd21504a2aa62134e41af56829c97585e0fa
Geass <geass1000@gmail.com>
2017-07-02 03:25:02 +0300

    Add: Modal module was added

--- src/app/actions/app.actions.ts
+++ src/app/actions/app.actions.ts
@@ -6,10 +6,38 @@ import { IArmory, IResources } from '../shared/interfaces/game.interface';
 @Injectable()
 export class AppActions {
 	static readonly CLASS_NAME = 'AppActions:';
+	static readonly OPEN_MODAL = AppActions.CLASS_NAME + 'OPEN_MODAL';
+	static readonly OPEN_PANEL = AppActions.CLASS_NAME + 'OPEN_PANEL';
+	static readonly CLOSE_ACTIVE_MODAL = AppActions.CLASS_NAME + 'CLOSE_ACTIVE_MODAL';
+
 	static readonly TOGGLE_FULL_WIDTH_MODE = AppActions.CLASS_NAME + 'TOGGLE_FULL_WIDTH_MODE';
 	static readonly SET_ACC = AppActions.CLASS_NAME + 'SET_ACC';
 	static readonly SET_RESOURCES = AppActions.CLASS_NAME + 'SET_RESOURCES';
 	static readonly SET_ARMORY = AppActions.CLASS_NAME + 'SET_ARMORY';
+
+	openModal (name : string, state : boolean = true) : IAction {
+    return {
+      type : AppActions.OPEN_MODAL,
+			payload : {
+				name : name,
+				state : state
+			}
+    };
+  }
+	openPanel (name : string, state : boolean = true) : IAction {
+    return {
+      type : AppActions.OPEN_PANEL,
+			payload : {
+				name : name,
+				state : state
+			}
+    };
+  }
+	closeActiveModal () : IAction {
+    return {
+      type : AppActions.CLOSE_ACTIVE_MODAL
+    };
+  }
 
 	setAcc (uid : string, auth_key : string, sid : string) : IAction {
     return {
