31d5a8b8ef28c678f766b94d0a7c06a35cb1f6b8
Geass <geass1000@gmail.com>
2017-07-20 02:28:18 +0300

    Chore: Errors were fixed

--- src/app/game-info/armory/armory.component.ts
+++ src/app/game-info/armory/armory.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import lang from '../../../assets/lang/lang';
+
 /* App Redux and Request */
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { NgRedux, select } from '@angular-redux/store';
 import { AppActions } from '../../actions/app.actions';
+
+/* App Interfaces and Classes */
+import { IRGameInfo, IResources, IArmory, ICostArmory, ICost, IRGameArmory, IGameArmory } from '../../shared/interfaces/game.interface';
 
 @Component({
   selector : 'as-armory',
@@ -13,6 +18,8 @@ import { AppActions } from '../../actions/app.actions';
   styleUrls : [ './armory.component.scss' ]
 })
 export class ArmoryComponent implements OnInit, OnDestroy {
+	public form : FormGroup;
+	public stateArmory : IArmory;
 
 	/* Redux */
 	private subscription : Array<Subscription> = [];
@@ -33,5 +40,15 @@ export class ArmoryComponent implements OnInit, OnDestroy {
 	 */
 	getUrlArmory (str : string) : string {
 		return `url('./assets/imgs/armory/${str}_big.png')`;
+	}
+
+	/**
+	 * getUrlResouce - выполняет формирование URL изображения ресурсов.
+	 *
+	 * @kind {event}
+	 * @return {void}
+	 */
+	getValue (str : string) : number {
+		return +str;
 	}
 }
