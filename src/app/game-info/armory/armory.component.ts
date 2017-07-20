d560e88426e6f007a4031288ac7d5e95c2d6c864
Geass <geass1000@gmail.com>
2017-07-20 16:27:55 +0300

    Chore: Component Armory was reworked

--- src/app/game-info/armory/armory.component.ts
+++ src/app/game-info/armory/armory.component.ts
@@ -9,8 +9,14 @@ import { Subscription } from 'rxjs/Subscription';
 import { NgRedux, select } from '@angular-redux/store';
 import { AppActions } from '../../actions/app.actions';
 
+/* App Services */
+import { LoggerService } from '../../core/logger.service';
+
 /* App Interfaces and Classes */
 import { IRGameInfo, IResources, IArmory, ICostArmory, ICost, IRGameArmory, IGameArmory } from '../../shared/interfaces/game.interface';
+
+/* App Validators */
+import { isNumber } from '../../shared/validators/is-number.validator';
 
 @Component({
   selector : 'as-armory',
@@ -18,19 +24,72 @@ import { IRGameInfo, IResources, IArmory, ICostArmory, ICost, IRGameArmory, IGam
   styleUrls : [ './armory.component.scss' ]
 })
 export class ArmoryComponent implements OnInit, OnDestroy {
+	/* Input */
+	@Input('dataType') dataType : string;
+
+	/* Public Variable */
 	public form : FormGroup;
-	public stateArmory : IArmory;
+	public current : number;
+	public result : number;
 
 	/* Redux */
 	private subscription : Array<Subscription> = [];
+	@select(['state', 'armory']) stateArmory$ : Observable<IArmory>;
+	public stateArmory : IArmory;
 
-  constructor() { }
+  constructor(private fb : FormBuilder,
+						 	private ngRedux : NgRedux<any>,
+						 	private appActions : AppActions,
+						 	private logger : LoggerService) { }
 
   ngOnInit() {
+		this.logger.info(`${this.constructor.name} - ngOnInit:`, this.dataType);
+		this.subscription.push(this.stateArmory$.subscribe((data) => {
+			this.stateArmory = Object.assign({}, data);
+			this.current = +this.stateArmory[this.dataType];
+			this.result = this.current;
+			this.ngOnDestroy();
+			this.buildForm();
+		}));
   }
 	ngOnDestroy () : void {
 		this.subscription.map((data) => data.unsubscribe());
 	}
+
+	/**
+	 * buildForm - method performs create form and one registration on form events.
+	 *
+	 * @function
+	 * @return {void}
+	 */
+	buildForm () : void {
+		if (!this.dataType) {
+			return ;
+		}
+		const fieldForm : { [key : string] : any } = {};
+		fieldForm[this.dataType] = [ '0', [ Validators.required, isNumber(false) ] ];
+
+		this.form = this.fb.group(fieldForm);
+
+		const sub : Subscription = this.form.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+		this.subscription.push(sub);
+
+    this.onValueChanged();
+	}
+
+	onValueChanged (data?: any) : void {
+		const methodName : string = 'onValueChanged';
+
+		if (this.form.invalid) {
+			this.logger.info(`${this.constructor.name} - ${methodName}:`, 'Form invalid');
+			this.result = this.current;
+			return;
+		}
+
+		const formValue : number = +this.form.get(this.dataType).value;
+		this.result = this.current + 5 * formValue;
+  }
 
 	/**
 	 * getUrlArmory - выполняет формирование URL изображения амуниции.
@@ -40,15 +99,5 @@ export class ArmoryComponent implements OnInit, OnDestroy {
 	 */
 	getUrlArmory (str : string) : string {
 		return `url('./assets/imgs/armory/${str}_big.png')`;
-	}
-
-	/**
-	 * getUrlResouce - выполняет формирование URL изображения ресурсов.
-	 *
-	 * @kind {event}
-	 * @return {void}
-	 */
-	getValue (str : string) : number {
-		return +str;
 	}
 }
