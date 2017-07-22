89b1cf256179800191fdf97b3262e5f00aef833a
Geass <geass1000@gmail.com>
2017-07-23 02:09:30 +0300

    Add: Resource component was added

--- src/app/game-info/game-info.module.ts
+++ src/app/game-info/game-info.module.ts
@@ -10,13 +10,14 @@ import { GameInfoRoutingModule } from './game-info-routing.module';
 /* App Feature - Component */
 import { GameInfoComponent } from './game-info.component';
 import { ArmoryComponent } from './armory/armory.component';
+import { ResourceComponent } from './resource/resource.component';
 
 /* App Feature - Service */
 
 
 @NgModule({
   imports:      [ SharedModule, GameInfoRoutingModule ],
-  declarations: [ GameInfoComponent, ArmoryComponent ],
+  declarations: [ GameInfoComponent, ArmoryComponent, ResourceComponent ],
   exports:      [ GameInfoComponent ],
   providers:    [  ]
 })
